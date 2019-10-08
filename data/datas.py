7072b68f1d71cb2d4e83b0d1a1d39203d96142e8
procopio matteo <procopio.matteo1@gmail.com>
2019-10-08 18:32:29 +0200

    commit data/datas.py

--- data/datas.py
+++ data/datas.py
@@ -5,4 +5,4 @@ class FileDownlod(object):
     def download(self):
 
         urllib.urlretrieve('https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/terminowe/synop/2019/'
-        , 'data/datas') 
+        , 'data/datas')
