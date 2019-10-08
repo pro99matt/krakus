d14bf29ad87d10511f6053095901d1fb7b872ffa
procopio matteo <procopio.matteo1@gmail.com>
2019-10-08 17:51:53 +0200

    tentativo x scaricare i dati

---
+++ data/datas.py
@@ -0,0 +1,8 @@
+import urllib
+
+class FileDownlod(object):
+
+    def download(self):
+
+        urllib.urlretrieve('https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/terminowe/synop/2019/'
+        , 'data/datas') 
